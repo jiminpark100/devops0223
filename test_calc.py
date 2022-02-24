0a57cb72a365e09c885d3c0493a1591fa428234b
jiminpark <pjm062716@gmail.com>
2022-02-24 14:14:41 +0900

    commit

--- test_calc.py
+++ test_calc.py
@@ -2,9 +2,10 @@ from unittest import result
 import unitest
 import calc
 
+
 class testCals(unitest.TestCase):
     def test_sum(self):
-        result=calc.sum(3, 7)
+        result = calc.sum(3, 7)
         self.assertEqual(10, result)
 
     def test_minus(self):
